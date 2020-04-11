f8410e36eb09f2212829f02e6dfa2a655e60190e
matt joseloff <msjo425@gmail.com>
2020-04-11 19:51:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,4 +20,5 @@ function sayHiToGrandma(string){
   return "I can't hear you!" }
   else if(string.toUpperCase()=== string){
   return "YES INDEED!" }
+  else if string === "I love you, Grandma"
 }
