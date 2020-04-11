e282a0543cdb8c5c4f975c0dd3f886dca34e38c3
matt joseloff <msjo425@gmail.com>
2020-04-11 19:12:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var lowercase = "hello!"
 
 function sayHiToGrandma(string){
   if (lowercase.toLowerCase() === lowercase){
-  return "I can't hear you!")
+  return "I can't hear you!"
 //  if(uppercase.toUpperCase()=== uppercase){
   //  return "YES INDEED!"
 //  }
