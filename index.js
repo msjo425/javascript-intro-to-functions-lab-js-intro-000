35882521ad4c8298123b59a8470e32c3b0f07b5f
matt joseloff <msjo425@gmail.com>
2020-04-11 19:47:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,5 @@ function sayHiToGrandma(string){
   if(lowercase.toLowerCase() === lowercase) {
   return "I can't hear you!" }
   else if(uppercase.toUpperCase()=== uppercase){
-  return "YES INDEED!"
-
-  }
+  return "YES INDEED!" }
 }
